e99df2f2c888f5f0c23ef5e8aafa35d88e73bb4b
sjud <64875465+sjud@users.noreply.github.com>
2024-01-25 23:02:32 -0500

    clippy fmt

--- dom_testing_library/src/lib.rs
+++ dom_testing_library/src/lib.rs
@@ -20,18 +20,18 @@ pub trait DomQuery {
     /// Seeget_by_text for an exact matcher.
     fn get_all_by_text_contains<S: AsRef<str>>(&self, text: S) -> Vec<TestElement>;
     /// Get an element by it's id, matches exactly. See get_by_id_contains for non-exact matching.
-    fn get_by_id<S: AsRef<str>>(&self, id: S) -> Result<TestElement,  GetOneError>;
+    fn get_by_id<S: AsRef<str>>(&self, id: S) -> Result<TestElement, GetOneError>;
     /// Get all elements by their id, matches exactly.
     fn get_all_by_id<S: AsRef<str>>(&self, id: S) -> Vec<TestElement>;
     /// Get an element whose id contains the text string, see get_by_id for exact id matching.
-    fn get_by_id_contains<S: AsRef<str>>(&self, id: S) -> Result<TestElement,  GetOneError>;
+    fn get_by_id_contains<S: AsRef<str>>(&self, id: S) -> Result<TestElement, GetOneError>;
     /// Get a list of elements whose id contains the text string, see get_by_id for exact id matching.
     fn get_all_by_id_contains<S: AsRef<str>>(&self, id: S) -> Vec<TestElement>;
     /// Get the element that is pointed to by a label whose text is the input of the method.
     /// i.e <label for="field">Btn</label><input id="field"/>
     /// With input of "field" would return the input whose id is field.
     /// If you want to find the label element itself, see get_by_text
-    fn get_by_label<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError>;
+    fn get_by_label<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError>;
     /// Get a list of  elements that are pointed to by a label whose text is the input of the method.
     /// i.e <label for="field">Btn</label><input id="field"/>
     /// With input of "field" would return the input whose id is field.
@@ -41,7 +41,7 @@ pub trait DomQuery {
     /// i.e <label for="field">Btn</label><input id="field"/>
     /// With input of "field" would return the input whose id is field.
     /// If you want to find the label element itself, see get_by_text
-    fn get_by_label_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError>;
+    fn get_by_label_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError>;
     /// Get a list of  elements that are pointed to by a label whose text is the input of the method.
     /// i.e <label for="field">Btn</label><input id="field"/>
     /// With input of "field" would return the input whose id is field.
@@ -50,13 +50,13 @@ pub trait DomQuery {
     /// Get elements whose display value is the exact match of this methods input.
     /// The elements that this method will find are: input, textarea, and select.
     /// This method will not match against items with non-display value attributes, i.e option, progress, li etc.
-    fn get_by_display_value<S: AsRef<str>>(&self, value: S) -> Result<TestElement,  GetOneError>;
+    fn get_by_display_value<S: AsRef<str>>(&self, value: S) -> Result<TestElement, GetOneError>;
     /// Get elements whose display value is the exact match of this methods input.
     /// The elements that this method will find are: input, textarea, and select.
     /// This method will not match against items with non-display value attributes, i.e option, progress, li etc.
     fn get_all_by_display_value<S: AsRef<str>>(&self, value: S) -> Vec<TestElement>;
     /// Get an element matching ARIA role.
-    fn get_by_role<S: AsRef<str>>(&self, role: S) -> Result<TestElement,  GetOneError>;
+    fn get_by_role<S: AsRef<str>>(&self, role: S) -> Result<TestElement, GetOneError>;
     /// Get a list of elements matching AIRA role.
     fn get_all_by_role<S: AsRef<str>>(&self, role: S) -> Vec<TestElement>;
     /// Get by placeholder text, checks textarea and input only. As those are the only applicable elements with placeholders.
@@ -64,7 +64,7 @@ pub trait DomQuery {
     fn get_by_placeholder_text<S: AsRef<str>>(
         &self,
         placeholder_text: S,
-    ) -> Result<TestElement,  GetOneError>;
+    ) -> Result<TestElement, GetOneError>;
     /// Get a list of elements by placeholder text, checks textarea and input only. As those are the only applicable elements with placeholders.
     /// Matches exactly.
     fn get_all_by_placeholder_text<S: AsRef<str>>(&self, placeholder_text: S) -> Vec<TestElement>;
@@ -73,7 +73,7 @@ pub trait DomQuery {
     fn get_by_placeholder_text_contains<S: AsRef<str>>(
         &self,
         placeholder_text: S,
-    ) -> Result<TestElement,  GetOneError>;
+    ) -> Result<TestElement, GetOneError>;
     /// Get a list of elements by placeholder text, checks textarea and input only. As those are the only applicable elements with placeholders.
     /// Checks if placeholder text contains method input.
     fn get_all_by_placeholder_text_contains<S: AsRef<str>>(
@@ -85,49 +85,47 @@ pub trait DomQuery {
 #[derive(Error, Debug, PartialEq)]
 pub enum GetOneError {
     #[error("Not Found:Attempting to find: {ident} by method {method}")]
-    NotFound {  method: &'static str,ident: String, },
+    NotFound { method: &'static str, ident: String },
     #[error("Found more than one element by method of get_{method} with input of {ident}, if you were expecting more than one match see the get_all_{method} version of this method instead.")]
     MoreThanOne { method: &'static str, ident: String },
 }
 
 impl GetOneError {
-    fn more_than_one(method:&'static str, ident:String) -> Self {
+    fn more_than_one(method: &'static str, ident: String) -> Self {
         Self::MoreThanOne { method, ident }
     }
-    fn not_found(method:&'static str, ident:String) -> Self {
+    fn not_found(method: &'static str, ident: String) -> Self {
         Self::NotFound { method, ident }
     }
 }
-pub trait GetOneErrorTrait{
+pub trait GetOneErrorTrait {
     fn is_not_found(&self) -> bool;
     fn is_more_than_one(&self) -> bool;
 }
 
-impl GetOneErrorTrait for GetOneError{
+impl GetOneErrorTrait for GetOneError {
     fn is_not_found(&self) -> bool {
         matches!(self, GetOneError::NotFound { .. })
     }
     fn is_more_than_one(&self) -> bool {
         matches!(self, GetOneError::MoreThanOne { .. })
-
     }
 }
 
-impl<T> GetOneErrorTrait for Result<T,GetOneError>{
+impl<T> GetOneErrorTrait for Result<T, GetOneError> {
     fn is_not_found(&self) -> bool {
         match &self {
             Ok(_) => false,
-            Err(err) => matches!(err, GetOneError::NotFound { .. })
+            Err(err) => matches!(err, GetOneError::NotFound { .. }),
         }
     }
     fn is_more_than_one(&self) -> bool {
         match &self {
             Ok(_) => false,
-            Err(err) => matches!(err, GetOneError::MoreThanOne { .. })
+            Err(err) => matches!(err, GetOneError::MoreThanOne { .. }),
         }
     }
 }
-
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct TestElement(pub HtmlElement);
@@ -175,9 +173,16 @@ impl Deref for DocumentWrapper<'_> {
         self.0
     }
 }
-fn get_one_inner<S: AsRef<str>>(list:Vec<TestElement>,method:&'static str, ident:S) -> Result<TestElement, GetOneError> {
+fn get_one_inner<S: AsRef<str>>(
+    list: Vec<TestElement>,
+    method: &'static str,
+    ident: S,
+) -> Result<TestElement, GetOneError> {
     if list.len() > 1 {
-        Err(GetOneError::more_than_one(method, ident.as_ref().to_string()))
+        Err(GetOneError::more_than_one(
+            method,
+            ident.as_ref().to_string(),
+        ))
     } else if list.is_empty() {
         Err(GetOneError::not_found(method, ident.as_ref().to_string()))
     } else {
@@ -186,11 +191,7 @@ fn get_one_inner<S: AsRef<str>>(list:Vec<TestElement>,method:&'static str, ident
 }
 impl DomQuery for DocumentWrapper<'_> {
     fn get_by_text<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
-        get_one_inner(
-            self.get_all_by_text(text.as_ref()),
-            "by_text",
-            text
-        )
+        get_one_inner(self.get_all_by_text(text.as_ref()), "by_text", text)
     }
     fn get_all_by_text<S: AsRef<str>>(&self, text: S) -> Vec<TestElement> {
         get_all_text_nodes(self.0)
@@ -200,13 +201,12 @@ impl DomQuery for DocumentWrapper<'_> {
             .collect()
     }
 
-    fn get_by_text_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_text_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
         get_one_inner(
             self.get_all_by_text_contains(text.as_ref()),
             "by_text_contains",
-            text
+            text,
         )
-
     }
 
     fn get_all_by_text_contains<S: AsRef<str>>(&self, text: S) -> Vec<TestElement> {
@@ -217,12 +217,8 @@ impl DomQuery for DocumentWrapper<'_> {
             .collect()
     }
 
-    fn get_by_id<S: AsRef<str>>(&self, id: S) -> Result<TestElement,  GetOneError> {
-        get_one_inner(
-            self.get_all_by_id(id.as_ref()),
-            "by_id",
-            id
-        )
+    fn get_by_id<S: AsRef<str>>(&self, id: S) -> Result<TestElement, GetOneError> {
+        get_one_inner(self.get_all_by_id(id.as_ref()), "by_id", id)
     }
     fn get_all_by_id<S: AsRef<str>>(&self, id: S) -> Vec<TestElement> {
         let tag_names = self.0.get_elements_by_tag_name("*");
@@ -230,9 +226,7 @@ impl DomQuery for DocumentWrapper<'_> {
         for i in 0..tag_names.length() {
             let item = tag_names.item(i).unwrap();
             if item.id() == id.as_ref() {
-                if let Ok(item) = item
-                    .dyn_into::<HtmlElement>()
-                {
+                if let Ok(item) = item.dyn_into::<HtmlElement>() {
                     list.push(item.into())
                 }
             }
@@ -240,11 +234,11 @@ impl DomQuery for DocumentWrapper<'_> {
         list
     }
 
-    fn get_by_id_contains<S: AsRef<str>>(&self, id: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_id_contains<S: AsRef<str>>(&self, id: S) -> Result<TestElement, GetOneError> {
         get_one_inner(
             self.get_all_by_id_contains(id.as_ref()),
             "by_id_contains",
-            id
+            id,
         )
     }
 
@@ -254,9 +248,7 @@ impl DomQuery for DocumentWrapper<'_> {
         for i in 0..tag_names.length() {
             let item = tag_names.item(i).unwrap();
             if item.id().contains(id.as_ref()) {
-                if let Ok(item) = item
-                    .dyn_into::<HtmlElement>()
-                {
+                if let Ok(item) = item.dyn_into::<HtmlElement>() {
                     list.push(item.into());
                 }
             }
@@ -264,12 +256,8 @@ impl DomQuery for DocumentWrapper<'_> {
         list
     }
 
-    fn get_by_label<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError> {
-        get_one_inner(
-            self.get_all_by_label(text.as_ref()),
-            "by_label",
-            text
-        )
+    fn get_by_label<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
+        get_one_inner(self.get_all_by_label(text.as_ref()), "by_label", text)
     }
 
     fn get_all_by_label<S: AsRef<str>>(&self, text: S) -> Vec<TestElement> {
@@ -289,11 +277,11 @@ impl DomQuery for DocumentWrapper<'_> {
         list
     }
 
-    fn get_by_label_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_label_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
         get_one_inner(
             self.get_all_by_label_contains(text.as_ref()),
             "by_label",
-            text
+            text,
         )
     }
 
@@ -314,11 +302,11 @@ impl DomQuery for DocumentWrapper<'_> {
         list
     }
 
-    fn get_by_display_value<S: AsRef<str>>(&self, value: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_display_value<S: AsRef<str>>(&self, value: S) -> Result<TestElement, GetOneError> {
         get_one_inner(
             self.get_all_by_display_value(value.as_ref()),
             "by_display_value",
-            value
+            value,
         )
     }
 
@@ -329,21 +317,17 @@ impl DomQuery for DocumentWrapper<'_> {
             .unwrap();
         let mut list = Vec::new();
         for i in 0..elements.length() {
-            let item = elements
-                .item(i)
-                .unwrap()
-                .dyn_into::<HtmlElement>()
-                .unwrap();
+            let item = elements.item(i).unwrap().dyn_into::<HtmlElement>().unwrap();
             if let Some(ref_item) = item.dyn_ref::<web_sys::HtmlTextAreaElement>() {
-                if &ref_item.value() == value.as_ref() {
+                if ref_item.value() == value.as_ref() {
                     list.push(TestElement(item));
                 }
             } else if let Some(ref_item) = item.dyn_ref::<web_sys::HtmlInputElement>() {
-                if &ref_item.value() == value.as_ref() {
+                if ref_item.value() == value.as_ref() {
                     list.push(TestElement(item));
                 }
             } else if let Ok(item) = item.dyn_into::<web_sys::HtmlSelectElement>() {
-                if &item.value() == value.as_ref() {
+                if item.value() == value.as_ref() {
                     list.push(TestElement(item.into()));
                 }
             }
@@ -351,12 +335,8 @@ impl DomQuery for DocumentWrapper<'_> {
         list
     }
 
-    fn get_by_role<S: AsRef<str>>(&self, role: S) -> Result<TestElement,  GetOneError> {
-        get_one_inner(
-            self.get_all_by_role(role.as_ref()),
-            "by_role",
-            role
-        )
+    fn get_by_role<S: AsRef<str>>(&self, role: S) -> Result<TestElement, GetOneError> {
+        get_one_inner(self.get_all_by_role(role.as_ref()), "by_role", role)
     }
 
     fn get_all_by_role<S: AsRef<str>>(&self, role: S) -> Vec<TestElement> {
@@ -379,11 +359,11 @@ impl DomQuery for DocumentWrapper<'_> {
     fn get_by_placeholder_text<S: AsRef<str>>(
         &self,
         placeholder_text: S,
-    ) -> Result<TestElement,  GetOneError> {
+    ) -> Result<TestElement, GetOneError> {
         get_one_inner(
             self.get_all_by_placeholder_text(placeholder_text.as_ref()),
             "by_role",
-            placeholder_text
+            placeholder_text,
         )
     }
 
@@ -392,17 +372,13 @@ impl DomQuery for DocumentWrapper<'_> {
         let mut list = Vec::new();
 
         for i in 0..elements.length() {
-            let item = elements
-                .item(i)
-                .unwrap()
-                .dyn_into::<HtmlElement>()
-                .unwrap();
+            let item = elements.item(i).unwrap().dyn_into::<HtmlElement>().unwrap();
             if let Some(ref_item) = item.dyn_ref::<web_sys::HtmlTextAreaElement>() {
-                if &ref_item.placeholder() == placeholder_text.as_ref() {
+                if ref_item.placeholder() == placeholder_text.as_ref() {
                     list.push(TestElement(item));
                 }
             } else if let Some(ref_item) = item.dyn_ref::<web_sys::HtmlInputElement>() {
-                if &ref_item.placeholder() == placeholder_text.as_ref() {
+                if ref_item.placeholder() == placeholder_text.as_ref() {
                     list.push(TestElement(item));
                 }
             }
@@ -413,11 +389,11 @@ impl DomQuery for DocumentWrapper<'_> {
     fn get_by_placeholder_text_contains<S: AsRef<str>>(
         &self,
         placeholder_text: S,
-    ) -> Result<TestElement,  GetOneError> {
+    ) -> Result<TestElement, GetOneError> {
         get_one_inner(
             self.get_all_by_placeholder_text_contains(placeholder_text.as_ref()),
             "by_role",
-            placeholder_text
+            placeholder_text,
         )
     }
 
@@ -429,11 +405,7 @@ impl DomQuery for DocumentWrapper<'_> {
         let mut list = Vec::new();
 
         for i in 0..elements.length() {
-            let item = elements
-                .item(i)
-                .unwrap()
-                .dyn_into::<HtmlElement>()
-                .unwrap();
+            let item = elements.item(i).unwrap().dyn_into::<HtmlElement>().unwrap();
 
             if let Some(ref_item) = item.dyn_ref::<web_sys::HtmlTextAreaElement>() {
                 if ref_item.placeholder().contains(placeholder_text.as_ref()) {
@@ -475,7 +447,7 @@ impl TextNodes {
         for node in self.0.iter() {
             if let Some(element) = node.parent_element() {
                 if let Some(html_element) = element.dyn_ref::<HtmlElement>() {
-                    if &html_element.inner_text() == text.as_ref() {
+                    if html_element.inner_text() == text.as_ref() {
                         list.push(html_element.clone());
                     }
                 }
@@ -519,7 +491,7 @@ where
         self.document().get_all_by_text(text)
     }
 
-    fn get_by_text_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_text_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_text_contains(text)
     }
 
@@ -527,7 +499,7 @@ where
         self.document().get_all_by_text_contains(text)
     }
 
-    fn get_by_id<S: AsRef<str>>(&self, id: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_id<S: AsRef<str>>(&self, id: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_id(id)
     }
 
@@ -535,7 +507,7 @@ where
         self.document().get_all_by_id(id)
     }
 
-    fn get_by_id_contains<S: AsRef<str>>(&self, id: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_id_contains<S: AsRef<str>>(&self, id: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_id_contains(id)
     }
 
@@ -543,7 +515,7 @@ where
         self.document().get_all_by_id_contains(id)
     }
 
-    fn get_by_label<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_label<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_label(text)
     }
 
@@ -551,7 +523,7 @@ where
         self.document().get_all_by_label(text)
     }
 
-    fn get_by_label_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_label_contains<S: AsRef<str>>(&self, text: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_label_contains(text)
     }
 
@@ -559,7 +531,7 @@ where
         self.document().get_all_by_label_contains(text)
     }
 
-    fn get_by_display_value<S: AsRef<str>>(&self, value: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_display_value<S: AsRef<str>>(&self, value: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_display_value(value)
     }
 
@@ -567,7 +539,7 @@ where
         self.document().get_all_by_display_value(value)
     }
 
-    fn get_by_role<S: AsRef<str>>(&self, role: S) -> Result<TestElement,  GetOneError> {
+    fn get_by_role<S: AsRef<str>>(&self, role: S) -> Result<TestElement, GetOneError> {
         self.document().get_by_role(role)
     }
 
@@ -578,7 +550,7 @@ where
     fn get_by_placeholder_text<S: AsRef<str>>(
         &self,
         placeholder_text: S,
-    ) -> Result<TestElement,  GetOneError> {
+    ) -> Result<TestElement, GetOneError> {
         self.document().get_by_placeholder_text(placeholder_text)
     }
 
@@ -590,7 +562,7 @@ where
     fn get_by_placeholder_text_contains<S: AsRef<str>>(
         &self,
         placeholder_text: S,
-    ) -> Result<TestElement,  GetOneError> {
+    ) -> Result<TestElement, GetOneError> {
         self.document()
             .get_by_placeholder_text_contains(placeholder_text)
     }
@@ -604,7 +576,7 @@ where
     }
 }
 
-#[cfg(all(test,feature="unit_tests"))]
+#[cfg(all(test, feature = "unit_tests"))]
 pub mod test {
     use super::*;
     use wasm_bindgen_test::*;
